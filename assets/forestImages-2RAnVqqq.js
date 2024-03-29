97c47670d15c484ccefe25810f858249883cbe27
IsaacIndex <isaacwongnh@gmail.com>
2024-03-29 15:44:45 +0800

    Updates

---
+++ assets/forestImages-2RAnVqqq.js
@@ -0,0 +1,1 @@
+const a="/snake-on-page/assets/map-yJkuYcxg.png",s="/snake-on-page/assets/map_achromatopsia-ScRW1R4c.png",n="/snake-on-page/assets/map_deuteranopia-hxK10b4I.png",o="/snake-on-page/assets/map_protanopia-pzQOl5d8.png",p="/snake-on-page/assets/map_tritanopia-xa2DqH0o.png",t={normal:a,achromatopsia:s,deuteranopia:n,protanopia:o,tritanopia:p};export{t as default};
