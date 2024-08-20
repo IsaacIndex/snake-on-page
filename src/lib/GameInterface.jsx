b14f7d92e65e68a33fe6400909cd84d448198acd
IsaacIndex <isaacwongnh@gmail.com>
2024-08-20 08:38:51 +0800

    Revert "revert build"
    This reverts commit 99cac586b084be470dd3ec09ad165395f923f95d.

--- src/lib/GameInterface.jsx
+++ src/lib/GameInterface.jsx
@@ -7,6 +7,20 @@ const GameInterface = () => {
   const [mapIndex, setMapIndex] = useState(0)
   const [score, setScore] = useState(0)
   console.log(score)
+<<<<<<< HEAD
+  // try {
+  //   console.log("public:" + process.env.PUBLIC_URL)
+  // } catch {
+  //   console.log("public failed")
+  // }
+  // try {
+  //   console.log("origin:" + window.location.origin)
+  // } catch {
+
+  //   console.log("origin failed")
+  // }
+=======
+>>>>>>> parent of 97dd32f (log base URL)
   const maps = {
     0: "forestImages",
     1: "desertImages",
@@ -16,6 +30,12 @@ const GameInterface = () => {
     console.log(score)
     setScore(prevScore => prevScore + 1)
     console.log(score)
+<<<<<<< HEAD
+=======
+
+    console.log("public:", process.env.PUBLIC_URL)
+    console.log("origin:", window.location.origin)
+>>>>>>> parent of 97dd32f (log base URL)
   }
 
   return (
