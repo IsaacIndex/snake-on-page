c11e6f918ba4a26e0b1dfc94bec2e27f5b474507
IsaacIndex <isaacwongnh@gmail.com>
2024-08-20 08:38:58 +0800

    Revert "comment out logging"
    This reverts commit 95fda3be66411dc763d0ef2ea6e8523a8de63556.

--- src/lib/GameInterface.jsx
+++ src/lib/GameInterface.jsx
@@ -7,17 +7,17 @@ const GameInterface = () => {
   const [mapIndex, setMapIndex] = useState(0)
   const [score, setScore] = useState(0)
   console.log(score)
-  // try {
-  //   console.log("public:" + process.env.PUBLIC_URL)
-  // } catch {
-  //   console.log("public failed")
-  // }
-  // try {
-  //   console.log("origin:" + window.location.origin)
-  // } catch {
+  try {
+    console.log("public:" + process.env.PUBLIC_URL)
+  } catch {
+    console.log("public failed")
+  }
+  try {
+    console.log("origin:" + window.location.origin)
+  } catch {
 
-  //   console.log("origin failed")
-  // }
+    console.log("origin failed")
+  }
   const maps = {
     0: "forestImages",
     1: "desertImages",
@@ -27,6 +27,7 @@ const GameInterface = () => {
     console.log(score)
     setScore(prevScore => prevScore + 1)
     console.log(score)
+
   }
 
   return (
