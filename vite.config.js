393ea8e224717e9252e24814fb9b3e525fc09425
IsaacIndex <isaacwongnh@gmail.com>
2025-06-14 17:44:31 +0800

    add dual mode vite config

--- vite.config.js
+++ vite.config.js
@@ -4,42 +4,46 @@ import { resolve } from 'path'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 1111
-  },
-  plugins: [
-    react(),
-    libInjectCss(),
-    // viteStaticCopy({
-    //   targets: [
-    //     { src: 'src/assets/*', dest: 'images' }
-    //   ]
-    // })
-  ],
-  build: {
-    cssCodeSplit: false,
-    sourcemap: process.env.NODE_ENV !== 'production',
-    copyPublicDir: true,
-    lib: {
-      entry: resolve(__dirname, 'src/lib/GameInterface.jsx'),
-      formats: ['es']
+export default defineConfig(({ mode }) => {
+  if (mode === 'gh') {
+    // Export 1: GitHub Pages
+    return {
+      base: "/snake-on-page/",
+      plugins: [react()],
+    }
+  }
+
+  // Export 2: Library mode
+  return {
+    server: {
+      port: 1111
     },
-    rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-      output: {
-        // assetFileNames: ({ name }) => {
-        //   if (name.startsWith('src/assets/')) {
-        //     return 'assets/[name][extname]';
-        //   }
-        //   return 'assets/[name]-[hash][extname]';
-        // },
-        globals: {
-          react: 'React',
+    plugins: [
+      react(),
+      libInjectCss(),
+      // viteStaticCopy({
+      //   targets: [
+      //     { src: 'src/assets/*', dest: 'images' }
+      //   ]
+      // })
+    ],
+    build: {
+      cssCodeSplit: false,
+      sourcemap: process.env.NODE_ENV !== 'production',
+      copyPublicDir: true,
+      lib: {
+        entry: resolve(__dirname, 'src/lib/GameInterface.jsx'),
+        formats: ['es']
+      },
+      rollupOptions: {
+        external: ['react', 'react/jsx-runtime'],
+        output: {
+          globals: {
+            react: 'React',
+          },
         },
       },
     },
-  },
-  publicDir: 'public'
+    publicDir: 'public'
+  }
 })
